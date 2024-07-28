4d8043b3992a0fd80485f5dc7d3203185b8f4ce5
andrewillerr <mcabkp333@gmail.com>
2024-07-29 00:50:37 +0700

    Updated

---
+++ Variable2.java
@@ -0,0 +1,8 @@
+public class Variable2 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        System.out.println("ค่าตัวเลขที่ 1 = "+num1);
+        System.out.println("ค่าตัวเลขที่ 2 = "+num2);
+    }
+}
