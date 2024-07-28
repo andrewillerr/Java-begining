4989255f9c4ad63faa7981a8d6f7624dbfadc272
andrewillerr <mcabkp333@gmail.com>
2024-07-29 00:26:50 +0700

    Your commit message

---
+++ Variable.java
@@ -0,0 +1,29 @@
+public class Variable {
+    public static void main(String[] args) {
+        //นิยามตัวแปร แบบไม่ระบุค่าเริ่มต้น
+        int number1;
+        boolean status;
+        char alphabet;
+        float number2;
+        double number3;
+
+
+        //นิยามตัวแปรแบบระบุค่าเริ่มต้น
+        int number01 = 10;
+        float number02 = 20.9f;  //float ต้องมี f ต่อท้าย
+        char alphabet01 = 'P';
+        boolean status01 = true;
+
+        /*การนิยามตัวแปรหลายบรรทัดในครั้งเดียว
+        int a;
+        int b;
+        int c;
+
+        จะได้
+        */
+
+        int a = 0, b = 1, c = 2;
+
+    }
+    
+}
