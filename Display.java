477b9e09498604211e3e9eac35d58808abfc3e77
andrewillerr <mcabkp333@gmail.com>
2024-07-28 23:12:12 +0700

    first commit

---
+++ Display.java
@@ -0,0 +1,8 @@
+public class Display {
+    public static void main(String[] args) {
+        System.out.println("The student is");
+        System.out.print("Prommin ");
+        System.out.print("Bkp");
+
+    }
+}
