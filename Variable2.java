3e1695f301e069372dfad2fedebcb2870af45a2a
andrewillerr <mcabkp333@gmail.com>
2024-07-29 01:00:56 +0700

    Updated

--- Variable2.java
+++ Variable2.java
@@ -1,8 +1,10 @@
 public class Variable2 {
     public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 5;
-        System.out.println("ค่าตัวเลขที่ 1 = "+num1);
-        System.out.println("ค่าตัวเลขที่ 2 = "+num2);
+        final int num1 = 10; //ห้ามเปลี่ยนค่า num1
+        int num2 = 5;  
+                       //ยังสามารถเปลี่ยนค่า num2 ได้
+        num2 = 15;
+        System.out.println("ค่าตัวเลขที่ 1 = "+num1);  //แสดงผล num1
+        System.out.println("ค่าตัวเลขที่ 2 = "+num2);  //แสดงผล num2 
     }
 }
