55fbd001584410d5dca25c5a767d91f8bd7c774e
andrewillerr <mcabkp333@gmail.com>
2024-07-29 00:19:06 +0700

    Updated files

--- Display.java
+++ Display.java
@@ -3,7 +3,13 @@ public class Display {
         // คำสั่งแสดงผลทางจอภาพ
         System.out.println("The student is");  //แสดงผลทางจอภาพแล้วขึ้นบรรทัดใหม่
         System.out.print("Prommin ");  //แสดงผลทางจอภาพปกติ
-        System.out.print("Bkpสา");
+        System.out.print("Bkp");
+
+         /*
+           แสดงผลข้อความทางจอภาพ
+           1. แบบขึ้นบรรทัดใหม่
+           2. แบบไม่ขึ้นบรรทัดใหม่
+         */
 
 
         
